3eb6a642a98410b5733dfa6e75b9161fb7fc2144
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-16 11:54:16 -0500

    inital movies route created

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
 
 var app = express();
 
@@ -16,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+
 
 module.exports = app;
