e9c67ba385279966e720a89827d66827d2847451
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-16 13:12:22 -0500

    completed read from movies table for /movies endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('knex')(require('../knexfile')['development']);
 
 router.get('/', function (req, res, next){
-    res.send('Hi');
-})
+    knex
+    .from('movies')
+    .select('*')
+    .then(movies => res.json(movies));
+});
 
 
 module.exports = router;
